42117347257c92eb111c3aa427439bb9a16f09f4
sharika472 <101918905+sharika472@users.noreply.github.com>
2022-03-28 15:31:21 +0530

    Add files via upload
    calculator application  addition file is added

---
+++ calc.py
@@ -0,0 +1,14 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+Output
+
