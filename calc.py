4d1d7ba2f400264414924028382aeeedf5a938af
sharika472 <101918905+sharika472@users.noreply.github.com>
2022-03-28 15:35:09 +0530

    Add files via upload
    calculator application subtraction file is added

--- calc.py
+++ calc.py
@@ -10,5 +10,5 @@ sum = num1 + num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-Output
-
+sub=num1-num2
+print('The sub of {0} and {1} is {2}'.format(num1, num2, sub))
